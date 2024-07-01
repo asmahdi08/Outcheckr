65c7c214a52536ded8502457f77ad9fc74ec3c14
Ashfaq Sadat <74450177+asmahdi08@users.noreply.github.com>
2024-07-01 12:40:08 +0600

    unfinsihed

--- Outcheckr.py
+++ Outcheckr.py
@@ -4,6 +4,7 @@ import argparse
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urljoin
+import os
 
 colorama.init()
 
@@ -41,7 +42,7 @@ parser = argparse.ArgumentParser("Argparser")
 parser.add_argument('-u', '--url', help = "url to check for", required=True)
 parser.add_argument('-n', '--no-color', help = "output without colors",required=False, action="store_true")
 parser.add_argument('-v', '--verbose', help = "Display results real-time", required=False, action="store_true")
-parser.add_argument('-o', '--output', help = "file to save the results", required=False)
+parser.add_argument('-o', '--output', help = "file to save the results", required=True)
 
 args = parser.parse_args()
 
@@ -65,8 +66,9 @@ checkingurl = args.url
 def get_outbound_links(domain):
         try:
             response = requests.get(domain)
+            response.raise_for_status()
 
-        except requests.RequestException:
+        except requests.RequestException as e:
             print(f"Error fetching {domain}: {e}")
             return []
         
@@ -91,24 +93,51 @@ def get_outbound_links(domain):
             return o_links
         else:
             return None
-        
-ols= get_outbound_links(checkingurl)
 
-def file_write(filename):
+def parse_input_file(filen):
+    file = open(f"./input/{filen}", "r")
+
+    inputs = file.readlines()
+
+    return inputs
+
+def file_write(filename, intype):
     if ".txt" in filename:
         pass
     else:
         filename = filename + ".txt"
-    file = open(filename, "w")
-    for link in ols:
-        file.write(link+"\n")
+    if intype == "single":
+        ols = get_outbound_links(args.url)
+        file = open(f"./output/{filename}", "w")
+        for link in ols:
+            file.write(link+"\n")
+    elif intype == "multiple":
+        inputlinks = parse_input_file(filen=args.url)
+        outall = open(f"./output/{filename}", "w")
+        for inlink in inputlinks:
+            ll = get_outbound_links(inlink)
+            for files in inputlinks:
+                os.mkdir(f"./output/(folder){filename}")
+                out = open(f"./output/(folder){filename}/{files}", "w")
+                for link in ll:
+                    out.write(link+"\n")
+                    outall.write(link+"\n")
 
 if args.output:
-    file_write(args.output)
-    print(f"{Green}successfully written in file!{Reset}\n\n")
+    if ".txt" in args.url:
+        file_write(filename=args.output,intype="multiple")
+    else:
+        file_write(filename=args.output,intype="single")
 
 if args.verbose:
-    for link in ols:
+    fn = args.output
+    if ".txt" in fn :
+        pass
+    else:
+        fn = fn+".txt"
+    outfile = open(f"./output/{fn}", "r")
+    outs = outfile.readlines()
+    for link in outs:
         print(link+"\n")
 
 
